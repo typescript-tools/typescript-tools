ea0eb5b10b74321c75451df1a2d02351a7be87a1
Eric Crosson <eric.s.crosson@utexas.edu>
2022-12-27 15:36:00 -0600

    chore(nix): create dev shell

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "@typescript-tools/typescript-tools";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+            };
+          };
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+          ];
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin:$PATH"
+            ${checks.pre-commit-check.shellHook}
+          '';
+        };
+      }
+    );
+}
